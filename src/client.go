b02ec20ba60566748c1b9dc525868ed7c6cac9f9
Yury Antonov <yantonov@yandex.ru>
2015-09-04 23:31:07 +0500

    baseUrl can be configured

--- src/client.go
+++ src/client.go
@@ -1,100 +1,109 @@
 package src
 
 import (
-    "encoding/json"
-    "errors"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "net/url"
-    "strings"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
-const basePath = "https://cloud-api.yandex.net/v1/disk"
-
 type Client struct {
-    token      string
-    HttpClient *http.Client
+	token      string
+	basePath   string
+	HttpClient *http.Client
 }
 
 func NewClient(token string, client ...*http.Client) *Client {
-    c := &Client{token: token}
-    if len(client) != 0 {
-        c.HttpClient = client[0]
-    } else {
-        c.HttpClient = http.DefaultClient
-    }
-    return c
+	return newClientInternal(
+		token,
+		"https://cloud-api.yandex.net/v1/disk",
+		client...)
+}
+
+func newClientInternal(token string, basePath string, client ...*http.Client) *Client {
+	c := &Client{
+		token:    token,
+		basePath: basePath,
+	}
+	if len(client) != 0 {
+		c.HttpClient = client[0]
+	} else {
+		c.HttpClient = http.DefaultClient
+	}
+	return c
 }
 
 type ErrorHandler func(*http.Response) error
 
 var defaultErrorHandler ErrorHandler = func(resp *http.Response) error {
-    if resp.StatusCode/100 == 5 {
-        return errors.New("server error")
-    }
+	if resp.StatusCode/100 == 5 {
+		return errors.New("server error")
+	}
 
-    if resp.StatusCode/100 == 4 {
-        var response DiskClientError
-        contents, _ := ioutil.ReadAll(resp.Body)
-        json.Unmarshal(contents, &response)
+	if resp.StatusCode/100 == 4 {
+		var response DiskClientError
+		contents, _ := ioutil.ReadAll(resp.Body)
+		json.Unmarshal(contents, &response)
 
-        return response
-    }
+		return response
+	}
 
-    if resp.StatusCode/100 == 3 {
-        return errors.New("redirect error")
-    }
-    return nil
+	if resp.StatusCode/100 == 3 {
+		return errors.New("redirect error")
+	}
+	return nil
 }
 
 func (client *Client) run(method, path string, params map[string]interface{}) ([]byte, error) {
-    var err error
+	var err error
 
-    values := make(url.Values)
-    for k, v := range params {
-        values.Set(k, fmt.Sprintf("%v", v))
-    }
+	values := make(url.Values)
+	for k, v := range params {
+		values.Set(k, fmt.Sprintf("%v", v))
+	}
 
-    var req *http.Request
-    if method == "POST" {
-        // TODO json serialize
-        req, err = http.NewRequest("POST", basePath+path, strings.NewReader(values.Encode()))
-        if err != nil {
-            return nil, err
-        }
-        req.Header.Set("Content-Type", "application/json")
-    } else {
-        req, err = http.NewRequest(method, basePath+path+"?"+values.Encode(), nil)
-        if err != nil {
-            return nil, err
-        }
-    }
+	var req *http.Request
+	if method == "POST" {
+		// TODO json serialize
+		req, err = http.NewRequest("POST", client.basePath+path, strings.NewReader(values.Encode()))
+		if err != nil {
+			return nil, err
+		}
+		req.Header.Set("Content-Type", "application/json")
+	} else {
+		req, err = http.NewRequest(method, client.basePath+path+"?"+values.Encode(), nil)
+		if err != nil {
+			return nil, err
+		}
+	}
 
-    return client.runRequest(req)
+	return client.runRequest(req)
 }
 
 func (client *Client) runRequest(req *http.Request) ([]byte, error) {
-    return client.runRequestWithErrorHandler(req, defaultErrorHandler)
+	return client.runRequestWithErrorHandler(req, defaultErrorHandler)
 }
 
 func (client *Client) runRequestWithErrorHandler(req *http.Request, errorHandler ErrorHandler) ([]byte, error) {
-    req.Header.Set("Authorization", "OAuth "+client.token)
-    resp, err := client.HttpClient.Do(req)
+	req.Header.Set("Authorization", "OAuth "+client.token)
+	resp, err := client.HttpClient.Do(req)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    return checkResponseForErrorsWithErrorHandler(resp, errorHandler)
+	return checkResponseForErrorsWithErrorHandler(resp, errorHandler)
 }
 
 func checkResponseForErrorsWithErrorHandler(resp *http.Response, errorHandler ErrorHandler) ([]byte, error) {
-    if resp.StatusCode/100 > 2 {
-        return nil, errorHandler(resp)
-    } else {
-        return ioutil.ReadAll(resp.Body)
-    }
+	if resp.StatusCode/100 > 2 {
+		return nil, errorHandler(resp)
+	} else {
+		return ioutil.ReadAll(resp.Body)
+	}
 }
